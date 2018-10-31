0f70f6a15336b06f2244342772ecfbabc21522ea
KieeRt <44137092+KieeRt@users.noreply.github.com>
2018-10-31 08:15:45 +0100

    Update and rename toFixed to toFixed.js

--- library/toFixed.js
+++ library/toFixed.js
@@ -1,4 +1,6 @@
 .toFixed()  method and pass it the number of decimal points you want to use
- For example, balance.toFixed(2) returns 325.00.
+ For example:
+ var balance = 325;
+ balance.toFixed(2)// returns 325.00.
  
  
