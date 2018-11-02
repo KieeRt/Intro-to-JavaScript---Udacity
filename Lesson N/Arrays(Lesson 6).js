772b90cd44286fb64ed25b3b46434e953c188e8f
KieeRt <44137092+KieeRt@users.noreply.github.com>
2018-11-02 09:16:20 +0100

    Create Arrays(Lesson 6).js

---
+++ Lesson N/Arrays(Lesson 6).js
@@ -0,0 +1,50 @@
+6) /*QUEST:
+Create an array called udaciFamily and add "Julia", "James", and your name to the array.
+Then, print the udaciFamily to the console using console.log.
+*/
+/*
+ * Programming Quiz: UdaciFamily (6-1)
+ */
+
+// your code goes here
+var udaciFamily=["Julia", "James", "YourName"];
+
+console.log(udaciFamily);
+-------------------------------------------------------------------------------------------
+7)
+/* QUEST:
+Create an array called crew to organize the Serenity’s crew and set it equal to the variables below .
+You don't need to type out the actual strings, just use the provided variables.
+*/
+/*
+ * Programming Quiz: Building the Crew (6-2)
+ */
+
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+var crew =[capital="Mal", second = "Zoe", pilot = "Wash", companion = "Inara", mercenary = "Jayne", mechanic = "Kaylee"];
+console.log(crew);
+-------------------------------------------------------------------------------------------
+8)
+/* QUEST:
+Starting with this array of prices, change the prices of the 1st, 3rd, and 7th elements in the array.
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+*/
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+prices[0] = 100;
+prices[2] = 0;
+prices [6] = 100000;
+
+console.log(prices);
+
