cb3459e445add091faa045d23c04273a54043725
KieeRt <44137092+KieeRt@users.noreply.github.com>
2018-11-03 09:28:31 +0100

    Update concat().js

--- library/concat().js
+++ library/concat().js
@@ -15,3 +15,8 @@ var numb3 = [7,8,9];
 var numbW = ["x", "y"];
 numbW.concat(numb1, numb2, numb3);
 //Output: ["x", "y", 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var numb1 = [1,2,3];
+var numb2 = [4,5,6];
+var numb3 = [7,8,9];
+var numb= numb1.concat(numb2, numb3);
+//Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
