e7da73e3403a3c1ec1df05f6d9751e548f3214a7
KieeRt <44137092+KieeRt@users.noreply.github.com>
2018-11-03 08:32:15 +0100

    Create .pop().js

---
+++ library/.pop().js
@@ -0,0 +1,8 @@
+Info: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop";
+array.pop();
+/*remove last element from array
+Example:
+*/
+var array =["book", "apple", "pen"];
+array.pop();
+//Output: [ book, apple]
