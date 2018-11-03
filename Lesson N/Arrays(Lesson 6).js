de1f237e58826dbcc26ebc69ff581110ae11781f
KieeRt <44137092+KieeRt@users.noreply.github.com>
2018-11-03 08:03:49 +0100

    Update Arrays(Lesson 6).js

--- Lesson N/Arrays(Lesson 6).js
+++ Lesson N/Arrays(Lesson 6).js
@@ -47,4 +47,37 @@ prices[2] = 0;
 prices [6] = 100000;
 
 console.log(prices);
+-------------------------------------------------------------------------------------------
+14)
+/*
+Directions:
+James was creating an array with the colors of the rainbow, and he forgot some colors. 
+The standard rainbow colors are usually listed in this order:
+
+var rainbow = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple"];
+but James had this:
+
+var rainbow = ["Red", "Orange", "Blackberry", "Blue"];
+Using only the splice() method, insert the missing colors into the array, and remove the color "Blackberry"
+by following these steps:
+
+Remove "Blackberry"
+Add "Yellow" and "Green"
+Add "Purple"
+*/
+/*
+ * Programming Quiz: Colors of the Rainbow (6-4)
+ *
+ * Use only the splice() method to modify the rainbow variable:
+ *  - remove "Blackberry"
+ *  - add "Yellow" and "Green"
+ *  - add "Purple"
+ */
+
+var rainbow = ['Red', 'Orange', 'Blackberry', 'Blue']; 
+rainbow.splice(2,1, "Yellow", "Green"); //Start from index 2, remove 1 element and add "Yello", "Green"
+rainbow.splice(5, 0, "Purple"); //Start from index 5, remove 0 element and add "Purple";
+console.log(rainbow);
+
+// your code goes here
 
