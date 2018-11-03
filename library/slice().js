babe6e8271af036e61559ade358e779ae89c17af
KieeRt <44137092+KieeRt@users.noreply.github.com>
2018-11-03 08:48:03 +0100

    Create slice().js

---
+++ library/slice().js
@@ -0,0 +1,15 @@
+INFO: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice"
+
+//Syntax: 
+array.slice(begin, end);
+
+/*The slice() method returns a shallow copy of a portion of an array into a new array 
+object selected from begin to end (end not included).
+The original array will not be modified.
+*/
+//Example:
+
+var array =["book", "apple", "pen"];
+array.slice(0,2);
+//Output: ["book", "apple"]
+
