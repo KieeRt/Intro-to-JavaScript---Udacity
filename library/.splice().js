a5cda3b3a4ec2c75edfa803056a3e01f8014b176
KieeRt <44137092+KieeRt@users.noreply.github.com>
2018-11-03 07:58:29 +0100

    Update .splice().js

--- library/.splice().js
+++ library/.splice().js
@@ -6,9 +6,11 @@ start = Index at which to start changing the array,  If negative, will begin tha
 and will be set to 0 if absolute value is greater than the length of the array.
 deleteCount = number from index that you wanna delete.
 itemN = item that you wanna and ad specific index
-*/
+
 Example:
+*/
 var array =["1", "two", "3"];
 array.splice(1, 1, "2"); 
 /*1)Go to index 1 (item on inted 1 is "two")
 2) remove 1 item ("two") and  add at index 1 new element  "3";
+*/
