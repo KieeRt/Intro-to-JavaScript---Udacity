e2c86fa9bcbfcbed691a6dc507ef741b1b1ea498
KieeRt <44137092+KieeRt@users.noreply.github.com>
2018-11-03 08:35:44 +0100

    Create shift().js

---
+++ library/shift().js
@@ -0,0 +1,7 @@
+Info: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/shift"
+array.shift()
+//Removes the first element from an array and returns that element.
+//Example:
+var array =["book", "apple", "pen"];
+array.shift();
+//Output: [apple, pen]
