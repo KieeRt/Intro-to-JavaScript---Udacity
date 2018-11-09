c9017bbba1ad31defd7e3453b9675436384b4593
KieeRt <44137092+KieeRt@users.noreply.github.com>
2018-11-09 23:36:53 +0100

    Update .pop().js

--- library/.pop().js
+++ library/.pop().js
@@ -1,8 +1,11 @@
 Info: "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/pop";
+
+//Syntax:
 array.pop();
-/*remove last element from array
-Example:
-*/
+//remove last element from array
+
+
+//Example:
 var array =["book", "apple", "pen"];
 array.pop();
 //Output: [ "book", "apple"]
