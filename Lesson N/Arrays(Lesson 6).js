8c41646466ed4d3f6c5ee2dbef7845c14b3edbce
Dmytro <44137092+KieeRt@users.noreply.github.com>
2019-02-05 21:54:15 +0100

    Update Arrays(Lesson 6).js

--- Lesson N/Arrays(Lesson 6).js
+++ Lesson N/Arrays(Lesson 6).js
@@ -151,12 +151,106 @@ console.log(crew);
 // your code goes here
 
 -------------------------------------------------------------------------------------------
- 
- 
+ 20)
+ Directions:
+Use the array's forEach() method to loop over the following array and add 100 to each of the values if the value is divisible by 3.
+ /*
+ * Programming Quiz: Another Type of Loop (6-8)
+ *
+ * Use the existing `test` variable and write a `forEach` loop
+ * that adds 100 to each number that is divisible by 3.
+ *
+ * Things to note:
+ *  - you must use an `if` statement to verify code is divisible by 3
+ *  - you can use `console.log` to verify the `test` variable when you're finished looping
+ */
+
+var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
+    19, 300, 3775, 299, 36, 209, 148, 169, 299,
+    6, 109, 20, 58, 139, 59, 3, 1, 139
+];
+
+test.forEach(function(number,index,array) {
+    if(number%3===0)
+        array[index]+=100;
+})
+
+console.log(test);
  
  -------------------------------------------------------------------------------------------
- 
+ 22)
+ Directions:
+Use the map() method to take the array of bill amounts shown below, and create a second array of
+numbers called totals that shows the bill amounts with a 15% tip added.
+/*
+ * Programming Quiz: I Got Bills (6-9)
+ *
+ * Use the .map() method to take the bills array below and create a second array
+ * of numbers called totals. The totals array should contains each amount from the
+ * bills array but with a 15% tip added. Log the totals array to the console.
+ *
+ * For example, the first two entries in the totals array would be:
+ *
+ * [57.76, 21.99, ... ]
+ *
+ * Things to note:
+ *  - each entry in the totals array must be a number
+ *  - each number must have an accuracy of two decimal points
+ */
+
+var bills = [50.23, 19.12, 34.01,
+    100.11, 12.15, 9.90, 29.11, 12.99,
+    10.00, 99.22, 102.20, 100.10, 6.77, 2.22
+];
+var totals = bills.map(function(x){
+    x=x*1.15;
+    x=x.toFixed(2);//Return string 
+    x=Number(x);//Return number
+    return x;
+    
+})
+console.log(totals);
+
  
  -------------------------------------------------------------------------------------------
- 
+ 25)
+Directions:
+Use a nested for loop to take the numbers array below and replace all of the values that are divisible by 2 (even numbers) 
+with the string "even" and all other numbers with the string "odd".
+/*
+ * Programming Quiz: Nested Numbers (6-10)
+ *
+ *   - The `numbers` variable is an array of arrays.
+ *   - Use a nested `for` loop to cycle through `numbers`.
+ *   - Convert each even number to the string "even"
+ *   - Convert each odd number to the string "odd"
+ */
+
+var numbers = [
+    [243, 12, 23, 12, 45, 45, 78, 66, 223, 3],
+    [34, 2, 1, 553, 23, 4, 66, 23, 4, 55],
+    [67, 56, 45, 553, 44, 55, 5, 428, 452, 3],
+    [12, 31, 55, 445, 79, 44, 674, 224, 4, 21],
+    [4, 2, 3, 52, 13, 51, 44, 1, 67, 5],
+    [5, 65, 4, 5, 5, 6, 5, 43, 23, 4424],
+    [74, 532, 6, 7, 35, 17, 89, 43, 43, 66],
+    [53, 6, 89, 10, 23, 52, 111, 44, 109, 80],
+    [67, 6, 53, 537, 2, 168, 16, 2, 1, 8],
+    [76, 7, 9, 6, 3, 73, 77, 100, 56, 100]
+];
+
+
+    for(var i = 0;i<numbers.length;i++){
+        for(var j = 0;j<numbers[i].length;j++){
+            if(numbers[i][j]%2===0){
+                numbers[i][j]='even';
+            }
+            else{
+                numbers[i][j]='odd';
+                
+            }
+        }
+    }
+   console.log(numbers);
+
  -------------------------------------------------------------------------------------------
