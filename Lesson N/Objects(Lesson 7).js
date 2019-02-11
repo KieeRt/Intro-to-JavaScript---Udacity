78705b24b00b675729ae24a0283d621e126f8be8
Dmytro <44137092+KieeRt@users.noreply.github.com>
2019-02-11 21:46:06 +0100

    Update Objects(Lesson 7).js

--- Lesson N/Objects(Lesson 7).js
+++ Lesson N/Objects(Lesson 7).js
@@ -44,3 +44,53 @@ var breakfast = {
     cost: 9.95,
     ingredients:["eggs", "sausage", "toast", "hashbrowns", "pancakes"]
 };
+--------------------------------------------------------------------------------------------
+9)
+Directions:
+Using the given object:
+
+var savingsAccount = {
+  balance: 1000,
+  interestRatePercent: 1,
+  deposit: function addMoney(amount) {
+    if (amount > 0) {
+      savingsAccount.balance += amount;
+    }
+  },
+  withdraw: function removeMoney(amount) {
+    var verifyBalance = savingsAccount.balance - amount;
+    if (amount > 0 && verifyBalance >= 0) {
+      savingsAccount.balance -= amount;
+    }
+  }
+};
+add a printAccountSummary() method that returns the following account message:
+
+Welcome!
+Your balance is currently $1000 and your interest rate is 1%.
+/*
+ * Programming Quiz: Bank Accounts 1 (7-3)
+ */
+
+var savingsAccount = {
+    balance: 1000,
+    interestRatePercent: 1,
+    deposit: function addMoney(amount) {
+        if (amount > 0) {
+            savingsAccount.balance += amount;
+        }
+    },
+    withdraw: function removeMoney(amount) {
+        var verifyBalance = savingsAccount.balance - amount;
+        if (amount > 0 && verifyBalance >= 0) {
+            savingsAccount.balance -= amount;
+        }
+    },
+    printAccountSummary: function Summary(){
+        return "Welcome!\nYour balance is currently $1000 and your interest rate is 1%.";
+    }
+};
+
+console.log(savingsAccount.printAccountSummary());
+
+
