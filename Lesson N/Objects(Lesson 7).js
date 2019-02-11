5ea5d9f9f87a4b386b8bd46b3ef469cfcf6b598b
Dmytro <44137092+KieeRt@users.noreply.github.com>
2019-02-11 22:11:36 +0100

    Update Objects(Lesson 7).js

--- Lesson N/Objects(Lesson 7).js
+++ Lesson N/Objects(Lesson 7).js
@@ -100,7 +100,7 @@ Create an object called facebookProfile. The object should have 3 properties:
 
 your name
 the number of friends you have, and
-an array of messages you've posted (as strings)
+an array of messages you have posted (as strings)
 The object should also have 4 methods:
 
 postMessage(message) - adds a new message string to the array
