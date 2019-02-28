ddb935d5e0dfc2d39f34c293bd197df721b4d87b
Dmytro <44137092+KieeRt@users.noreply.github.com>
2019-02-28 19:20:30 +0100

    Update Arrays(Lesson 6).js

--- Lesson N/Arrays(Lesson 6).js
+++ Lesson N/Arrays(Lesson 6).js
@@ -152,6 +152,7 @@ console.log(crew);
 
 -------------------------------------------------------------------------------------------
  20)
+ /*
  Directions:
 Use the array's forEach() method to loop over the following array and add 100 to each of the values if the value is divisible by 3.
  /*
